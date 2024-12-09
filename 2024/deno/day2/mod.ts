6158612f00a42e6815c453b45b362a52363d2d2f
Dima Budaragin <d.budaragin+dev@gmail.com>
2024-12-09 01:58:54 +0000

    Add day2

---
+++ 2024/deno/day2/mod.ts
@@ -0,0 +1,39 @@
+async function parse(input: ReadableStream<string>) {
+  return (await Array.fromAsync(input)).map((line) =>
+    line.split(/\s+/).map((x) => parseInt(x, 10))
+  );
+}
+
+function isSafe(report: number[]) {
+  let safe = true;
+  let up = false, down = false;
+  let prev = report[0];
+  for (const cur of report.slice(1)) {
+    const diff = cur - prev;
+    if (Math.abs(diff) < 1 || Math.abs(diff) > 3) {
+      safe = false;
+      break;
+    }
+    if (diff > 0) up = true;
+    if (diff < 0) down = true;
+    prev = cur;
+  }
+  safe &&= up && !down || down && !up;
+  return safe;
+}
+
+export async function part1(input: ReadableStream<string>) {
+  return (await parse(input)).filter(isSafe).length;
+}
+
+function getVariations(a: number[]): Array<number[]> {
+  return [a].concat(
+    a.map((_, i) => a.slice(0, i).concat(a.slice(i + 1))),
+  );
+}
+
+export async function part2(input: ReadableStream<string>) {
+  return (await parse(input))
+    .filter((report) => getVariations(report).some(isSafe))
+    .length;
+}
---
+++ 2024/deno/day2/test.ts
@@ -0,0 +1,43 @@
+import { assertEquals } from "jsr:@std/assert";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
+import { dedent } from "jsr:@qnighy/dedent";
+
+import { part1, part2 } from "./mod.ts";
+
+Deno.test(async function examplePart1() {
+  assertEquals(
+    await part1(from`\
+      7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9
+    `),
+    2,
+  );
+});
+
+Deno.test(async function examplePart2() {
+  assertEquals(
+    await part2(from`\
+      7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9
+    `),
+    4,
+  );
+});
+
+export function from(
+  strings: TemplateStringsArray,
+  // deno-lint-ignore no-explicit-any
+  ...keys: any[]
+): ReadableStream<string> {
+  return ReadableStream.from(
+    [dedent(strings, ...keys)],
+  ).pipeThrough(new TextLineStream());
+}
