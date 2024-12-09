d3fe753d8cd69b8277c3c40156b5db9bf1a05c01
Dima Budaragin <d.budaragin+dev@gmail.com>
2024-12-09 03:37:39 +0000

    Add day5

---
+++ 2024/deno/day5/mod.ts
@@ -0,0 +1,63 @@
+import { sum } from "jsr:@es-toolkit/es-toolkit";
+
+async function parse(
+  input: ReadableStream<string>,
+): Promise<[Map<number, Set<number>>, number[][]]> {
+  const lines = await Array.fromAsync(input);
+  const empty = lines.indexOf("");
+
+  const deps = new Map<number, Set<number>>();
+  lines.slice(0, empty)
+    .map((line) => line.split("|"))
+    .map((pair) => pair.map((x) => parseInt(x, 10)))
+    .forEach(([a, b]) => {
+      if (deps.has(a)) deps.get(a)?.add(b);
+      else deps.set(a, new Set([b]));
+    });
+
+  const reports = lines.slice(empty + 1)
+    .map((line) => line.split(","))
+    .map((parts) => parts.map((x) => parseInt(x, 10)));
+
+  return [deps, reports];
+}
+
+const isValid = (deps: Map<number, Set<number>>) =>
+  function (report: number[]): boolean {
+    const printed = new Set();
+    for (const a of report) {
+      if (printed.intersection(deps.get(a) ?? new Set()).size > 0) {
+        return false;
+      }
+      printed.add(a);
+    }
+    return true;
+  };
+
+export async function part1(input: ReadableStream<string>) {
+  const [deps, reports] = await parse(input);
+  return sum(
+    reports
+      .filter(isValid(deps))
+      .map((report) => report[Math.floor(report.length / 2)]),
+  );
+}
+
+export async function part2(input: ReadableStream<string>) {
+  const [deps, reports] = await parse(input);
+  const depsOf = (a: number) => deps.get(a) ?? new Set();
+
+  return sum(
+    reports
+      .filter((report) => !isValid(deps)(report))
+      .map((report) => {
+        report.sort((a, b) => {
+          if (depsOf(a).has(b)) return -1;
+          if (depsOf(b).has(a)) return 1;
+          return 0;
+        });
+        return report;
+      })
+      .map((report) => report[Math.floor(report.length / 2)]),
+  );
+}
---
+++ 2024/deno/day5/test.ts
@@ -0,0 +1,87 @@
+import { assertEquals } from "jsr:@std/assert";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
+import { dedent } from "jsr:@qnighy/dedent";
+
+import { part1, part2 } from "./mod.ts";
+
+Deno.test(async function examplePart1() {
+  assertEquals(
+    await part1(from`\
+      47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47
+    `),
+    143,
+  );
+});
+
+Deno.test(async function examplePart2() {
+  assertEquals(
+    await part2(from`\
+      47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47
+    `),
+    123,
+  );
+});
+
+export function from(
+  strings: TemplateStringsArray,
+  // deno-lint-ignore no-explicit-any
+  ...keys: any[]
+): ReadableStream<string> {
+  return ReadableStream.from(
+    [dedent(strings, ...keys)],
+  ).pipeThrough(new TextLineStream());
+}
