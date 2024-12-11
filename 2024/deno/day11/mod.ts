aaf99899906044d98988f8410329a0a316afba6d
Dima Budaragin <d.budaragin+dev@gmail.com>
2024-12-11 06:11:36 +0000

    Add day11

---
+++ 2024/deno/day11/mod.ts
@@ -0,0 +1,66 @@
+import { toText } from "jsr:@std/streams/to-text";
+import { sum } from "jsr:@es-toolkit/es-toolkit";
+
+async function parse(input: ReadableStream<string>) {
+  return (await toText(input)).split(/\s+/).map((x) => parseInt(x, 10));
+}
+
+function count(n: number, depth: number): number {
+  if (depth === 0) return 1;
+
+  if (n === 0) return count(1, depth - 1);
+
+  const s = n.toString();
+  if (s.length % 2 === 0) {
+    const l = parseInt(s.slice(0, s.length / 2), 10);
+    const r = parseInt(s.slice(s.length / 2), 10);
+    return count(l, depth - 1) + count(r, depth - 1);
+  } else return count(n * 2024, depth - 1);
+}
+
+export async function part1(input: ReadableStream<string>) {
+  const numbers = await parse(input);
+  const answer = sum(numbers.map((n) => count(n, 25)));
+  return answer;
+}
+
+const add = (m: Map<number, number>, n: number, k: number = 1) =>
+  m.set(n, (m.get(n) ?? 0) + k);
+
+function init(numbers: number[]): Map<number, number> {
+  const m = new Map();
+  for (const n of numbers) add(m, n, 1);
+  return m;
+}
+
+function iterate(numbers: Map<number, number>): Map<number, number> {
+  const m = new Map();
+  for (const [n, k] of numbers) {
+    if (n === 0) {
+      add(m, 1, k);
+    } else {
+      const s = n.toString();
+      if (s.length % 2 === 0) {
+        const l = parseInt(s.slice(0, s.length / 2), 10);
+        add(m, l, k);
+        const r = parseInt(s.slice(s.length / 2), 10);
+        add(m, r, k);
+      } else {
+        add(m, n * 2024, k);
+      }
+    }
+  }
+  return m;
+}
+
+export async function part2(input: ReadableStream<string>) {
+  const numbers = await parse(input);
+
+  let counts = init(numbers);
+  for (let i = 0; i < 75; i++) {
+    counts = iterate(counts);
+  }
+
+  const answer = sum(counts.values().toArray());
+  return answer;
+}
---
+++ 2024/deno/day11/test.ts
@@ -0,0 +1,33 @@
+import { assertEquals } from "jsr:@std/assert";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
+import { dedent } from "jsr:@qnighy/dedent";
+
+import { part1, part2 } from "./mod.ts";
+
+Deno.test(async function examplePart1() {
+  assertEquals(
+    await part1(from`\
+      125 17
+    `),
+    55312,
+  );
+});
+
+Deno.test(async function examplePart2() {
+  assertEquals(
+    await part2(from`\
+      125 17
+    `),
+    65601038650482,
+  );
+});
+
+export function from(
+  strings: TemplateStringsArray,
+  // deno-lint-ignore no-explicit-any
+  ...keys: any[]
+): ReadableStream<string> {
+  return ReadableStream.from(
+    [dedent(strings, ...keys)],
+  ).pipeThrough(new TextLineStream());
+}
