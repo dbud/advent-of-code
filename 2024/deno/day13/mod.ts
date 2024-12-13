393cd940c405cf77cc5a408688c39b60748ededa
Dima Budaragin <d.budaragin+dev@gmail.com>
2024-12-13 10:38:28 +0000

    Add day13

---
+++ 2024/deno/day13/mod.ts
@@ -0,0 +1,58 @@
+import { chunk, take } from "jsr:@es-toolkit/es-toolkit";
+import { None, Option, Some } from "jsr:@seacrest/option-result-rs";
+
+async function parse(input: ReadableStream<string>) {
+  return chunk(await Array.fromAsync(input), 4)
+    .map((block) =>
+      take(block, 3)
+        .map((line: string) => {
+          const [_1, x, _2, y] = line.split(/\+|=|,/);
+          return [x, y].map(Number);
+        })
+    );
+}
+
+const divrem = (a: number, b: number) => [Math.floor(a / b), a % b];
+
+function solve(
+  ax: number,
+  ay: number,
+  bx: number,
+  by: number,
+  x: number,
+  y: number,
+): Option<[number, number]> {
+  // s * ax + t * bx = x
+  // s * ay + t * by = y
+  const d = ax * by - bx * ay;
+  const ds = x * by - y * bx;
+  const dt = -x * ay + y * ax;
+  const [s, srem] = divrem(ds, d);
+  const [t, trem] = divrem(dt, d);
+  if (srem === 0 && trem === 0 && s >= 0 && t >= 0) {
+    return Some([s, t]);
+  } else {
+    return None();
+  }
+}
+
+export async function part1(input: ReadableStream<string>) {
+  let cost = 0;
+  for (const [[ax, ay], [bx, by], [x, y]] of await parse(input)) {
+    solve(ax, ay, bx, by, x, y).map(([s, t]) => {
+      cost += s * 3 + t;
+    });
+  }
+  return cost;
+}
+
+export async function part2(input: ReadableStream<string>) {
+  const offset = 10000000000000;
+  let cost = 0;
+  for (const [[ax, ay], [bx, by], [x, y]] of await parse(input)) {
+    solve(ax, ay, bx, by, x + offset, y + offset).map(([s, t]) => {
+      cost += s * 3 + t;
+    });
+  }
+  return cost;
+}
---
+++ 2024/deno/day13/test.ts
@@ -0,0 +1,61 @@
+import { assertEquals } from "jsr:@std/assert";
+import { TextLineStream } from "jsr:@std/streams/text-line-stream";
+import { dedent } from "jsr:@qnighy/dedent";
+
+import { part1, part2 } from "./mod.ts";
+
+Deno.test(async function examplePart1() {
+  assertEquals(
+    await part1(from`\
+      Button A: X+94, Y+34
+      Button B: X+22, Y+67
+      Prize: X=8400, Y=5400
+
+      Button A: X+26, Y+66
+      Button B: X+67, Y+21
+      Prize: X=12748, Y=12176
+
+      Button A: X+17, Y+86
+      Button B: X+84, Y+37
+      Prize: X=7870, Y=6450
+
+      Button A: X+69, Y+23
+      Button B: X+27, Y+71
+      Prize: X=18641, Y=10279
+    `),
+    480,
+  );
+});
+
+Deno.test(async function examplePart2() {
+  assertEquals(
+    await part2(from`\
+      Button A: X+94, Y+34
+      Button B: X+22, Y+67
+      Prize: X=8400, Y=5400
+
+      Button A: X+26, Y+66
+      Button B: X+67, Y+21
+      Prize: X=12748, Y=12176
+
+      Button A: X+17, Y+86
+      Button B: X+84, Y+37
+      Prize: X=7870, Y=6450
+
+      Button A: X+69, Y+23
+      Button B: X+27, Y+71
+      Prize: X=18641, Y=10279
+    `),
+    875318608908,
+  );
+});
+
+export function from(
+  strings: TemplateStringsArray,
+  // deno-lint-ignore no-explicit-any
+  ...keys: any[]
+): ReadableStream<string> {
+  return ReadableStream.from(
+    [dedent(strings, ...keys)],
+  ).pipeThrough(new TextLineStream());
+}
